352b7ed80046e6639f23844a4b5eed6d97a7f4b2
Sagarchauhan-99 <157916055+Sagarchauhan-99@users.noreply.github.com>
2024-07-16 16:10:21 +0530

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,20 @@
+with cte as(
+select * from bike_share_yr_0
+union all
+select * from bike_share_yr_1)
+
+select 
+dteday,
+season,
+a.yr,
+weekday,
+hr,
+rider_type,
+riders,
+price,
+COGS,
+riders*price as Revenue,
+riders*price-COGS as profit
+from cte a
+left join cost_table b
+on a.yr=b.yr
